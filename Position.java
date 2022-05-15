c4862de2d887e807dfa56dd7db86bee505acd800
MO MKOGA <mohamedimohamedi308@gmail.com>
2022-05-15 15:54:01 +0300

    2020-04-07610

---
+++ Position.java
@@ -0,0 +1,21 @@
+public class Position {
+    import java.math.RoundingMode;
+import java.text.DecimalFormat;
+    public class position {
+        public static void main(String[] args) {
+            double a=9.81;
+            double t=10;
+            double vi=0;
+            double xi=0 ;
+            double position;
+            position=0.5*a*t*t+vi*t+xi;
+
+            DecimalFormat df=new DecimalFormat("#.##");
+            df.setRoundingMode(RoundingMode.CEILING);
+            System.out.print("the position is:  ");
+            System.out.print(df.format(position));
+
+        }
+    }
+
+}
